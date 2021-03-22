cf94ffe1d90cfbff0ea81727185fa0e43ac1cea9
Marco Nenciarini <marco.nenciarini@enterprisedb.com>
2021-03-22 12:29:16 +0100

    Review

--- .github/generate-strategy.sh
+++ .github/generate-strategy.sh
@@ -2,50 +2,51 @@
 set -eu
 
 declare -A aliases=(
-  [13]='latest'
-  [9.6]='9'
+	[13]='latest'
+	[9.6]='9'
 )
 
-#self="$(basename "${BASH_SOURCE[0]}")"
 cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}/..")")"
 
-versions=($(ls -d */ | grep -v ^src/))
+for version in */; do
+	[[ $version == src/ ]] && continue
+	versions+=("$version")
+done
 versions=("${versions[@]%/}")
 
 # sort version numbers with highest first
-IFS=$'\n'
-versions=($(echo "${versions[*]}" | sort -rV))
-unset IFS
+mapfile -t versions < <(IFS=$'\n'; sort -rV <<< "${versions[*]}")
 
 # prints "$2$1$3$1...$N"
 join() {
-  local sep="$1"
-  shift
-  local out
-  printf -v out "${sep//%/%%}%s" "$@"
-  echo "${out#$sep}"
+	local sep="$1"
+	shift
+	local out
+	printf -v out "${sep//%/%%}%s" "$@"
+	echo "${out#$sep}"
 }
 
 entries=()
 for version in "${versions[@]}"; do
 
-  fullVersion=$(jq -r '.POSTGRES_VERSION' "${version}/versions.json" | sed -e 's/-.*$//g')
-  releaseVersion=$(jq -r '.IMAGE_RELEASE_VERSION' "${version}/versions.json")
+	versionFile="${version}/.versions.json"
+	fullVersion=$(jq -r '.POSTGRES_VERSION | split("-") | .[0]' "${versionFile}")
+	releaseVersion=$(jq -r '.IMAGE_RELEASE_VERSION' "${versionFile}")
 
-  versionAliases=()
-  while [ "$fullVersion" != "$version" -a "${fullVersion%[.-]*}" != "$fullVersion" ]; do
-    versionAliases+=("${fullVersion}"-"${releaseVersion}")
-    fullVersion="${fullVersion%[.-]*}"
-  done
+	versionAliases=()
+	while [ "$fullVersion" != "$version" ] && [ "${fullVersion%[.-]*}" != "$fullVersion" ]; do
+		versionAliases+=("${fullVersion}"-"${releaseVersion}")
+		fullVersion="${fullVersion%[.-]*}"
+	done
 
-  versionAliases+=(
-    "${version}"
-    ${aliases[$version]:+"${aliases[$version]}"}
-  )
+	versionAliases+=(
+		"${version}"
+		${aliases[$version]:+"${aliases[$version]}"}
+	)
 
-  entries+=(
-    "{\"version\": \"$version\", \"tags\": [\"$(join "\", \"" "${versionAliases[@]}")\"]}"
-  )
+	entries+=(
+		"{\"version\": \"$version\", \"tags\": [\"$(join "\", \"" "${versionAliases[@]}")\"]}"
+	)
 done
 
 strategy="{\"fail-fast\": false, \"matrix\": {\"include\": [$(join ', ' "${entries[@]}")]}}"
