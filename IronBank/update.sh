5659627b198bfa952bf0e05c6f55bc307ba73dc7
Niccolò Fei <niccolo.fei@enterprisedb.com>
2023-01-04 15:11:18 +0100

    chore: Remove IronBank pip hack
    Signed-off-by: Niccolò Fei <niccolo.fei@enterprisedb.com>

--- IronBank/update.sh
+++ IronBank/update.sh
@@ -220,8 +220,6 @@ update_requirements() {
 	barmanVersion=$(get_latest_barman_version)
 	# If there's a new version we need to recreate the requirements files
 	echo "barman[cloud,azure,snappy,google] == $barmanVersion" > requirements.in
-	# ugly hack; not very proud of this
-	echo "pip == 21.3.1" >> requirements.in
 
 	# This will take the requirements.in file and generate a file
 	# requirements.txt with the hashes for the required packages
