834895de75ff5e4a1fea0b1d90014bc2c7f32b19
Jonathan Gonzalez V. <jonathan.gonzalez@enterprisedb.com>
2024-12-19 10:23:28 +0100

    Revert "fix: skip barman version 3.12.0 (#198)" (#201)
    This reverts commit e4c3cdac215564e24a37cf83878e99a402be0c54.
    
    Signed-off-by: Jonathan Gonzalez V. <jonathan.gonzalez@enterprisedb.com>

--- UBI/update.sh
+++ UBI/update.sh
@@ -127,8 +127,7 @@ compare_architecture_pkgs() {
 # Get the latest Barman version
 latest_barman_version=
 _raw_get_latest_barman_version() {
-#	curl -s https://pypi.org/pypi/barman/json | jq -r '.releases | keys[]' | sort -Vr | head -n1
-	echo "3.11.1"
+	curl -s https://pypi.org/pypi/barman/json | jq -r '.releases | keys[]' | sort -Vr | head -n1
 }
 get_latest_barman_version() {
 	if [ -z "$latest_barman_version" ]; then
